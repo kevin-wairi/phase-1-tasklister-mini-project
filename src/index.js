fe47abd58707aa3532c1fcb83e749eba0697f43e
kevin Wairi <kevinwairi@gmail.com>
2023-03-25 01:27:36 +0300

    Assignment Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded',()=>{
+ let form =  document.getElementById('create-task-form');
+form.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  taskLister(e.target.new_task_description.value)
+  form.reset()
+})
+
+
+
+
+function taskLister(taskList){
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click',deleteTask)
+  li.textContent=taskList
+  li.appendChild(btn)
+  btn.textContent = 'X'
+  document.getElementById('tasks').appendChild(li)
+  
+  console.log(btn)
+  console.log(taskList)
+  
+}
+})
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
+
+
+
+
